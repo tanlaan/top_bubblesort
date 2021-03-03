07359324476b91ff188d96f5f2cf27338a8a571a
Chris Bolas <tanlaan33@gmail.com>
2021-03-02 21:04:23 -0800

    Passed TOP test case

--- spec/bubblesort_test.rb
+++ spec/bubblesort_test.rb
@@ -15,4 +15,9 @@ RSpec.describe 'Bubble Sort Example' do
       expect(bubble_sort([2,1,3,1])).to eq([1,1,2,3])
     end
   end
+  describe 'The Odin Project Example' do
+    it 'Can pass their test case' do
+      expect(bubble_sort([4,3,78,2,0,2])).to eq([0,2,2,3,4,78])
+    end
+  end
 end
